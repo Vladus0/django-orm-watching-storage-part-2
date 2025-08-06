330e85f6c7270e4dd491474f7a462eca7488d4f3
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2025-08-06 18:43:33 +0400

    Сделал правки

--- format_duration.py
+++ format_duration.py
@@ -1,8 +1,9 @@
 def format_duration(duration):
+    total_seconds = duration
     seconds_in_hour = 3600
     minutes_in_hour = 60
     seconds_in_minute = 60
-    hours = int(duration // seconds_in_hour)
-    minutes = int((duration % seconds_in_hour) // minutes_in_hour)
-    seconds = int(((duration % seconds_in_hour) % minutes_in_hour) // seconds_in_minute)
+    hours = int(total_seconds // seconds_in_hour)
+    minutes = int((total_seconds % seconds_in_hour) // minutes_in_hour)
+    seconds = int(total_seconds % seconds_in_minute)
     return f"{hours:02}:{minutes:02}:{seconds:02}"
